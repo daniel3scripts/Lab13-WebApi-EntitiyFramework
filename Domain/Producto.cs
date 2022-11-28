44a28827da1793195774de356703ddba6dcf9403
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-11-28 06:01:40 -0500

    commit01

---
+++ Domain/Producto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+
+{
+
+    
+    public class Producto
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public decimal SalePrice { get; set; }
+        
+        public decimal IGV { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+        
+       
+        
+        
+
+    }
+    //public Producto prod = new Producto()
+    //{
+
+    //    IGV = SalePrice * 0.18
+        
+
+
+    //};
+    //public abstract class Calculos
+    //{
+
+    //    public Producto prod = new Producto()
+    //    {
+
+    //        IGV = 0
+
+    //    };
+    //}
+    //public abstract decimal Calculo(decimal amount)
+    //{
+    //    amount = SalePrice * 0.18;
+    //    return amount;
+
+    //}
+
+
+
+
+
+}
---
+++ Infraestructure/Migrations/202211281030105_v2 - create product.cs
@@ -0,0 +1,32 @@
+﻿namespace Infraestructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2createproduct : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productoes",
+                c => new
+                    {
+                        ProductID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Description = c.String(),
+                        SalePrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        IGV = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CreationDate = c.DateTime(nullable: false),
+                        IsActive = c.Boolean(nullable: false),
+                        ExpiryDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Productoes");
+        }
+    }
+}

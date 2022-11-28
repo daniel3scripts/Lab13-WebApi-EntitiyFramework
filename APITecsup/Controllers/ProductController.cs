60e001100e07cfe2300edb094a1786eced6ee889
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-11-28 17:23:28 -0500

    commit02

---
+++ APITecsup/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Service;
+using Domain;
+using System.Threading.Tasks;
+
+namespace APITecsup.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+        public List<Producto> GetProducts()
+        {
+            var service = new ProductService();
+            var products = service.GetProducts();
+
+            return products;
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        //public void Post([FromBody]string value)
+        //{
+
+        //}
+        //public async Task<Producto> InsertCalculo(Producto factura)
+        //{
+            
+        //    factura.IGV = factura.SalePrice * 0.18M;
+            
+        //    var service = new ProductService();
+        //    service.InsertProducts( factura);
+        //    //await Where(x => x.IsActive= true).ToList();
+                
+        //    return factura;
+        //}
+
+
+
+        [HttpPost]
+        public bool InsertProducts(Producto producto)
+        {
+            var respo = true;
+            try
+            {
+                var service = new ProductService();
+                service.InsertProducts(new Producto
+                {
+                    Name = producto.Name,
+                    Description = producto.Description,
+                    SalePrice = producto.SalePrice,
+                    IGV= producto.IGV,
+                    ExpiryDate =producto.ExpiryDate
+                    
+
+            });   
+            }
+            catch (Exception )
+            {
+
+                respo = false;
+                //throw e;
+            }
+            return respo;
+        }
+
+        
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Infraestructure;
+
+namespace Service
+{
+    public class ProductService
+    {
+        public List<Producto> GetProducts()
+        
+        {
+            using (var contextito = new ExampleContext())
+            {
+                return contextito.Productos.Where(z => z.IsActive == true).ToList();
+            }
+                //return new List<Producto>(); 
+        }
+        public Producto GetProductsById(int id)
+        {
+            using(var contextito = new ExampleContext())
+            {
+                return contextito.Productos.Find(id);
+            }
+        }
+
+        public void InsertProducts(Producto producto)
+        {
+            using( var contextito = new ExampleContext())
+            {
+                producto.IsActive = true;
+                producto.CreationDate = DateTime.Now;
+                //producto.ExpiryDate = DateTime.Today;
+                producto.IGV = producto.SalePrice * 0.18M;
+                contextito.Productos.Add(producto);
+                contextito.SaveChanges();
+            }
+        }
+         
+    }
+}
